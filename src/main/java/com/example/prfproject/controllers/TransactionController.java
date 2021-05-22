3e0fb28b49e070f89f3de4073cf4b1524abe896b
Fodor Ádám <Fodor.Adam.1@stud.u-szege.hu>
2021-05-22 22:27:22 +0200

    changes before deploy

--- src/main/java/com/example/prfproject/controllers/TransactionController.java
+++ src/main/java/com/example/prfproject/controllers/TransactionController.java
@@ -12,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping
+@CrossOrigin(origins = "*")
 public class TransactionController {
     TransactionService transactionService;
 
